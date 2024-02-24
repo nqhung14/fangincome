2ceb890edbacf47b97a1c06c9e1d9af6a78b7999
Hung Nguyen Quoc <hunguyen@absolute.com>
2024-02-24 10:45:31 +0700

    first update

---
+++ src/main/java/com/fang/fangincome/FangIncomeApplication.java
@@ -0,0 +1,13 @@
+package com.fang.fangincome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FangIncomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FangIncomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fang/fangincome/FangIncomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fang.fangincome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FangIncomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
